ad5010c57e0c70bef1657b097f68eb118b763f6d
Taufik12b <taufikansari12b@gmail.com>
2023-11-16 15:27:52 +0530

    16 nov
    hiurv

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,3 @@
-
 const showExpenseForm = document.getElementById("showExpenseForm");
 const expenseForm = document.getElementById("expenseForm");
 const expenseEntryForm = document.getElementById("expenseEntryForm");
@@ -27,3 +26,46 @@ statusSelect.addEventListener("change", () => {
 });
 
 document.getElementById("cancel").addEventListener("click", hideExpenseForm);
+
+ // document.getElementById("submit").addEventListener("click", () => {
+    //     const category = document.getElementById("category").value;
+    //     const total_expense = document.getElementById("total_expense").value;
+    //     const issued_date = document.getElementById("issued_date").value;
+    //     const status = document.getElementById("status").value;
+
+    //     if (category && total_expense && issued_date && status) {
+    //         // Create a new expense entry
+    //         const newRow = document.createElement('tr');
+    //         newRow.innerHTML = `
+    //             <td>#${entryCount}</td>
+    //             <td>
+    //                 <div class="client">
+    //                     <div class="client-img bg-img" style="background-image: url('img/1.jpeg')"></div>
+    //                     <div class="client-info">
+    //                         <h4>Arunabh Shikhar</h4>
+    //                         <small>${category}</small>
+    //                         </div>
+    //                         </div>
+    //             </td>
+    //             <td>Rs ${total_expense}</td>
+    //             <td>${issued_date}</td>
+    //             <td>${status}</td>
+    //             <td>
+    //                 <div class="actions">
+    //                     <span class="lab la-telegram-plane"></span>
+    //                     <span class="las la-eye"></span>
+    //                     <span class="las la-ellipsis-v"></span>
+    //                     </div>
+    //                     </td>
+    //                     `;
+    //                     entryCount++;
+                        
+    //                     // Append the new row to the table body
+    //                     expenseTableBody.appendChild(newRow);
+
+    //         // Reset the form and hide it
+    //         hideExpenseForm();
+    //     } else {
+    //         alert("Please fill in all the details.");
+    //     }
+    // });
--- dashboard.php
+++ dashboard.php
@@ -47,62 +47,7 @@ $result = mysqli_num_rows($query1);
 
 <html>
 <style>
-    body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-            position: relative;
-        }
 
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            position: fixed;
-            z-index:78979;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-        }
-
-        #expenseForm {
-            background-color: rgba(255, 255, 255, 0.9);
-            border: 1px solid #ccc;
-            padding: 20px;
-            text-align: center;
-            max-width: 400px;
-        }
-
-        #paidAmountContainer {
-            display: none;
-        }
-
-        #expenseTable {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        #expenseTable th, #expenseTable td {
-            border: 1px solid #ccc;
-            padding: 10px;
-        }
-
-        .client {
-            display: flex;
-            align-items: center;
-        }
-
-        .client-img {
-            width: 50px;
-            height: 50px;
-            background-size: cover;
-            border-radius: 50%;
-            margin-right: 10px;
-        }
-
-        .client-info h4 {
-            margin: 0;
-        }
     
 </style>
 
@@ -530,81 +475,6 @@ $result = mysqli_num_rows($query1);
 
 
 </body>
-
-<script>
-    const showExpenseForm = document.getElementById("showExpenseForm");
-    const expenseForm = document.getElementById("expenseForm");
-    const expenseEntryForm = document.getElementById("expenseEntryForm");
-    const statusSelect = document.getElementById("status");
-    const paidAmountContainer = document.getElementById("paidAmountContainer");
-    const expenseTableBody = document.getElementById("expenseTableBody");
-    let entryCount = 1;
-    
-    showExpenseForm.addEventListener("click", () => {
-        showExpenseForm.style.display = "none"; // Hide the button
-        expenseForm.style.display = "block";
-    });
-
-    const hideExpenseForm = () => {
-        showExpenseForm.style.display = "block"; // Show the button
-        expenseForm.style.display = "none";
-        expenseEntryForm.reset();
-    };
-
-    statusSelect.addEventListener("change", () => {
-        if (statusSelect.value === "partiallyPaid") {
-            paidAmountContainer.style.display = "block";
-        } else {
-            paidAmountContainer.style.display = "none";
-        }
-    });
-
-    document.getElementById("cancel").addEventListener("click", hideExpenseForm);
-    
-    // document.getElementById("submit").addEventListener("click", () => {
-    //     const category = document.getElementById("category").value;
-    //     const total_expense = document.getElementById("total_expense").value;
-    //     const issued_date = document.getElementById("issued_date").value;
-    //     const status = document.getElementById("status").value;
-
-    //     if (category && total_expense && issued_date && status) {
-    //         // Create a new expense entry
-    //         const newRow = document.createElement('tr');
-    //         newRow.innerHTML = `
-    //             <td>#${entryCount}</td>
-    //             <td>
-    //                 <div class="client">
-    //                     <div class="client-img bg-img" style="background-image: url('img/1.jpeg')"></div>
-    //                     <div class="client-info">
-    //                         <h4>Arunabh Shikhar</h4>
-    //                         <small>${category}</small>
-    //                         </div>
-    //                         </div>
-    //             </td>
-    //             <td>Rs ${total_expense}</td>
-    //             <td>${issued_date}</td>
-    //             <td>${status}</td>
-    //             <td>
-    //                 <div class="actions">
-    //                     <span class="lab la-telegram-plane"></span>
-    //                     <span class="las la-eye"></span>
-    //                     <span class="las la-ellipsis-v"></span>
-    //                     </div>
-    //                     </td>
-    //                     `;
-    //                     entryCount++;
-                        
-    //                     // Append the new row to the table body
-    //                     expenseTableBody.appendChild(newRow);
-
-    //         // Reset the form and hide it
-    //         hideExpenseForm();
-    //     } else {
-    //         alert("Please fill in all the details.");
-    //     }
-    // });
-</script>
-
 
 
 </html>
